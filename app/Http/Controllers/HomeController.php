0c63776d9c03ad7e607cb0904af8f653ee24ee2d
DylanB <dylanbergmann25@gmail.com>
2023-10-04 14:29:35 +0700

    added activity right sidebar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Room;
 use App\Models\Topic;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,10 +12,12 @@ class HomeController extends Controller
     public function index() {
         $topics = Topic::all()->take(5);
         $rooms = Room::with('host', 'participants', 'topic')->get();
+        $messages = Message::with('user', 'room')->take(5)->get();
 
         return view('core.index', [
             'topics' => $topics,
-            'rooms' => $rooms
+            'rooms' => $rooms,
+            'messages' => $messages,
         ]);
     }
 }
